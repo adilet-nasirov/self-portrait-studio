f7a4dac797b6af38ec17cf77dc50943b5431c8d4
Chynara Baichaeva <baichaeva.c@gmail.com>
2022-01-13 11:07:18 -0600

    I have displayed all dresses on dresses page, finished css for dresses

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 "use strict";
 import { getData } from "./data.js";
 const data = getData();
+
+//VARIABLES
+const main = document.querySelector(".main");
+
+//Function to display all products
+const displayProducts = function (arr) {
+  arr.forEach((el) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardDiv.innerHTML = `
+      <div class="product-img-container">
+        <img class="product-img1" src=${el.image} alt="">
+        <img class="product-img1" src=${el.image2} alt="">
+      </div>
+      <div class="product-info">
+        <p class="product-name">${el.name}</p>
+        <p class="product-detail">${el.style}, ${el.length}</p>
+        <p class="product-price">${el.price}</p>
+      </div>`;
+    main.appendChild(cardDiv);
+  });
+};
+displayProducts(data); //calling this function to display all dresses in the very beginning
